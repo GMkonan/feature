e265214c25781a1c770cc75819d06df86c2e7274
GMkonan <guilhemont@hotmail.com>
2023-03-29 07:57:55 -0300

    Add echo version

--- src/airplane/install.sh
+++ src/airplane/install.sh
@@ -6,6 +6,8 @@ rm -rf /var/lib/apt/lists/*
 
 AIRPLANE_VERSION=${VERSION:-"latest"}
 
+echo "Airplane CLI version: ${AIRPLANE_VERSION}"
+
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
